555be5f86d13a52f8b6ab27e8c74256f0975d950
Damian Baar <damian.baar@gmail.com>
2019-12-30 15:03:41 +0100

    extra todo

--- shell.nix
+++ shell.nix
@@ -6,6 +6,9 @@ mkShell {
   # TODO bazel-watch
   # TODO generate-poms -> bazel command
   buildInputs = [bazel bash maven jdk bazel-buildtools];
+
+  # TODO find all pom.xml
+  # for d in $RUNFILES/*/bin; do PATH="$PATH:$d"; done
   shellHook = ''
     echo elo!
     bazel build //...
